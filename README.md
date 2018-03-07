12779ca9443a6ca7bd0b046b7ea246de3ab2e881
Danny <dannyliang.726@berkeley.edu>
2018-03-06 19:08:05 -0800

    Updated to online version

---
+++ README.md
@@ -0,0 +1,2 @@
+# Code for my portfolio page
+## Last updated July 2017
--- updatedSite/index.js
+++ updatedSite/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(e) {
   $( "#Welcome" ).fadeIn(1500),
     $( "#Welcome" ).fadeOut(1500, function() {
       $('body').vidbg({
-         'mp4': 'http://dannyliang.com/images/final.mp4',
+         'mp4': 'https://dannyliang.com/images/final.mp4',
         }, {
                                   // Options
           muted: true,
@@ -21,6 +21,7 @@ $(document).ready(function(e) {
       })
 
 function name_animate(name, vid, text) {
+  $( ".title" ).clearQueue();
   if ($( "#name" ).html() !== name) {
     $('body').vidbg({
                      'mp4': vid,
@@ -32,8 +33,13 @@ function name_animate(name, vid, text) {
                         overlayColor: '#000',
                         overlayAlpha: '0.3',
                       }),
-    $( ".title" ).animate({left: "-15em", opacity: "0"}, 700),
-
+               
+    $( ".title" ).animate({left: "-15em", opacity: "0"}, 700);
+    if (name === "Graphing March Madness") {
+      $( "#name" ).css("font-size", "1.7em");
+    } else {
+      $( "#name" ).css("font-size", "2em");
+    }
     $( "#name" ).text(name),
     $( ".title" ).animate({left: "3em", opacity: "1"}, 700),
     $( "#AboutText" ).fadeOut(700, function() {
@@ -50,7 +56,7 @@ $( "#GMM" ).hover(function() {
         $( "#GTD" ).css("opacity", "0.6"),
           $( "#GTL" ).css("opacity", "0.4"),
             $( "#Blog" ).css("opacity", "0.2"),
-              name_animate("Graphing March Madness", 'http://dannyliang.com/images/Sunset-Lapse.mp4', "Graphing March Madness is a site with visualizations of college basketball's yearly tournament. The graphics are created using d3.js.");
+              name_animate("Graphing March Madness", 'https://dannyliang.com/assets/CBB.mp4', "Graphing March Madness is a site with visualizations of college basketball's yearly tournament. The graphics are created using d3.js.");
 }, function() {
       $( ".header" ).animate({right: "1em"}, 300);
 
@@ -62,7 +68,7 @@ $( "#111" ).hover(function() {
         $( "#GTD" ).css("opacity", "0.8"),
           $( "#GTL" ).css("opacity", "0.6"),
             $( "#Blog" ).css("opacity", "0.4"),
-              name_animate("NBA Survey", 'http://dannyliang.com/images/Sunset-Lapse.mp4', "111 is a network of sites based on a NBA survey I conducted in the Summer of 2017. It contains the survey itself, the results, and further analysis.");
+              name_animate("NBA Survey", 'https://dannyliang.com/assets/NBA.mp4', "111 is a network of sites based on a NBA survey I conducted in the Summer of 2017. It contains the survey itself, the results, and further analysis.");
 }, function() {
   $( ".header" ).animate({right: "2em"}, 300);
 })
@@ -73,7 +79,7 @@ $( "#GTD" ).hover(function() {
         $( "#GTD" ).css("opacity", "1.0"),
           $( "#GTL" ).css("opacity", "0.8"),
             $( "#Blog" ).css("opacity", "0.6"),
-              name_animate("Games to Diamond", "http://dannyliang.com/images/Cheer-Up.mp4", "Games to Diamond is a webapp built using the Flask microframework. It runs simulations to approximate how many games a user would have to play to reach their desired rank in League of Legends.")
+              name_animate("Games to Diamond", "https://dannyliang.com/assets/League.mp4", "Games to Diamond is a webapp built using the Flask microframework. It runs simulations to approximate how many games a user would have to play to reach their desired rank in League of Legends.")
 }, function() {
   $( ".header" ).animate({right: "3em"}, 300);
 })
@@ -84,7 +90,7 @@ $( "#GTL" ).hover(function() {
         $( "#GTD" ).css("opacity", "0.8"),
           $( "#GTL" ).css("opacity", "1.0"),
             $( "#Blog" ).css("opacity", "0.8"),
-              name_animate("Games to Legend", "http://dannyliang.com/images/Blurry-Lights.mp4", "Games to Legend is a webapp built using the Flask microframework. It runs simulations to approximate how many games a user would have to play to reach their desired rank in Hearthstone.")
+              name_animate("Games to Legend", "https://dannyliang.com/assets/Hearthstone.mp4", "Games to Legend is a webapp built using the Flask microframework. It runs simulations to approximate how many games a user would have to play to reach their desired rank in Hearthstone.")
 }, function() {
     $( ".header" ).animate({right: "4em"}, 300);
 })
@@ -95,17 +101,17 @@ $( "#Blog" ).hover(function() {
         $( "#GTD" ).css("opacity", "0.6"),
           $( "#GTL" ).css("opacity", "0.8"),
             $( "#Blog" ).css("opacity", "1.0"),
-              name_animate("Blog", "http://dannyliang.com/images/Sunset-Lapse.mp4", "A blog with my thoughts on the NBA and the analysis of the results of my results.")
+              name_animate("Blog", "https://dannyliang.com/images/Sunset-Lapse.mp4", "A blog with my thoughts on the NBA and the analysis of the results of my results.")
 }, function() {
   $( ".header" ).animate({right: "4em"}, 300);
 })
 
 $("#GMM").click(function() {
-  window.location.href = "http://dannyliang.com/GMM.html";
+  window.location.href = "https://dannyliang.com/GMM.html";
 });
 
 $("#111").click(function() {
-  window.location.href = "http://dannyliang.com/111.html";
+  window.location.href = "https://dannyliang.com/111.html";
 });
 
 $("#GTD").click(function() {
@@ -117,18 +123,20 @@ $("#GTL").click(function() {
 })
 
 $("#Resume").click(function() {
-  window.location.href = "http://dannyliang.com/assets/Resume1.pdf";
+  window.location.href = "https://dannyliang.com/assets/ResumeSoph.pdf";
 })
 
 $("#Blog").click(function() {
-  window.location.href = "http://dannyliang.com/111/Blog.html";
+  window.location.href = "https://dannyliang.com/111/Blog.html";
 })
 
 $("#Spotify").click(function() {
-  $("#playlist").fadeIn(500);
+  $("#playlist").toggle(500),
+  $( "#AboutText" ).fadeOut(500);
 })
 
 $("#About").click(function() {
   $( "#AboutText").text("I'm a sophomore at UC Berkeley studying Computer Science and Economics, who enjoys watching and playing basketball, analytics, cooking, and LeBron James. My personal projects closest to completion are available on the sidebar. Thanks for visiting! ")
-  $( "#AboutText" ).toggle(500);
+  $( "#AboutText" ).toggle(500),
+  $( "#playlist" ).fadeOut(500);
 })
